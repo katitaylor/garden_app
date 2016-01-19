08dff4434f2998f721134157714124dafa1ffe3b
David Keathley <keathleydavidj@gmail.com>
2016-01-19 15:51:41 -0600

    unnecessary changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119164010) do
+ActiveRecord::Schema.define(version: 20160119181720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "comment_id"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["comment_id"], name: "index_comments_on_comment_id", using: :btree
-
   create_table "gardens", force: :cascade do |t|
-    t.integer  "garden_id"
     t.string   "name",         null: false
     t.string   "address"
     t.string   "city"
@@ -37,19 +33,14 @@ ActiveRecord::Schema.define(version: 20160119164010) do
     t.datetime "updated_at"
   end
 
-  add_index "gardens", ["garden_id"], name: "index_gardens_on_garden_id", using: :btree
-
   create_table "plant_types", force: :cascade do |t|
-    t.integer "plant_type_id"
-    t.string  "plant_name"
-    t.string  "scientific_name"
+    t.string "plant_name"
+    t.string "scientific_name"
   end
 
   add_index "plant_types", ["plant_name"], name: "index_plant_types_on_plant_name", using: :btree
-  add_index "plant_types", ["plant_type_id"], name: "index_plant_types_on_plant_type_id", using: :btree
 
   create_table "plants", force: :cascade do |t|
-    t.integer  "plant_id"
     t.string   "name_of_plant"
     t.string   "date_planted"
     t.string   "date_ended"
@@ -58,22 +49,30 @@ ActiveRecord::Schema.define(version: 20160119164010) do
     t.datetime "updated_at"
   end
 
-  add_index "plants", ["plant_id"], name: "index_plants_on_plant_id", using: :btree
-
   create_table "users", force: :cascade do |t|
-    t.string   "first_name", null: false
+    t.string   "first_name",                          null: false
     t.string   "last_name"
     t.string   "address"
     t.string   "city"
-    t.integer  "zip",        null: false
-    t.string   "username",   null: false
-    t.string   "email",      null: false
+    t.integer  "zip",                                 null: false
+    t.string   "username",                            null: false
     t.string   "avatar_url"
     t.text     "about_me"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
