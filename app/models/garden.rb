e694733b41fc34740b24954ca92f95516e8095a1
kati taylor <katherine.pelletier@gmail.com>
2016-01-19 11:35:46 -0600

    models

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,2 +1,6 @@
 class Garden < ActiveRecord::Base
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :address, length: { maximum: 50 }
+  validates :city,  length: { maximum: 50 }
+  validates :zip, length: { is: 5 }
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,4 @@
 class Plant < ActiveRecord::Base
+  validates :name_of_plant, length: { maximum: 50 }
+
 end
