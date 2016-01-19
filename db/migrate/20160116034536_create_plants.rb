9c48b10bb877b1073f25cd717e3c04ce6acedeff
David Keathley <keathleydavidj@gmail.com>
2016-01-19 09:55:58 -0600

    fixed typo

--- db/migrate/20160116034536_create_plants.rb
+++ db/migrate/20160116034536_create_plants.rb
@@ -1,7 +1,7 @@
 class CreatePlants < ActiveRecord::Migration
   def change
     create_table :plants do |t|
-      t.string :data_planted
+      t.string :date_planted
       t.string :date_ended
       t.boolean :plant_again?
 
