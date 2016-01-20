9f156c71790d735c85bf612efa41bf546d076926
Metel Patel <metelpatel@gmail.com>
2016-01-20 17:05:17 -0600

    nothing new

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    'homes/index' # Or :prefix_to_your_route
+    'users/index' # Or :prefix_to_your_route
   end
 end
