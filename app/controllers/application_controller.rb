d5f709a4bef003a910a31a53aeb30b6758aaf5e2
David Keathley <keathleydavidj@gmail.com>
2016-01-21 10:30:11 -0600

    got redirect problem taken care of

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    before_filter :configure_permitted_parameters, if: :devise_controller?
+	protected
 
-protected
-
-  def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) do |u|
-  u.permit(:email, :username, :password, :password_confirmation)
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :first_name, :last_name, :address, :city, :zip, :avatar_url) }
+	end
 end
-end
-end
-
