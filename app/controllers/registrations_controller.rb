3b7113c8a17afb9221651a68e595ef1e71906cb2
David Keathley <keathleydavidj@gmail.com>
2016-01-21 15:23:41 -0600

    fixed dropdown menu

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,17 +1,17 @@
+
 class RegistrationsController < Devise::RegistrationsController
   def update
   end
 
   protected
 
-<<<<<<< HEAD
-  def after_sign_up_path_for(resource)
-    'users/index' # Or :prefix_to_your_route
-  end
-end
-=======
+
+#   def after_sign_up_path_for(resource)
+#     'users/index' # Or :prefix_to_your_route
+#   end
+# end
+
   def after_inactive_sign_up_path_for(resource)
   	'/home/index'
   end 
 end
->>>>>>> jess-thurs-monning2
