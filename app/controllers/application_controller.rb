966423b20d0906083e28fd37c4c809cb63989eae
David Keathley <keathleydavidj@gmail.com>
2016-01-22 11:43:28 -0600

    added about me field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
 	def configure_permitted_parameters
 	  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
 
-	  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :first_name, :last_name, :address, :city, :zip, :avatar_url) }
+	  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :first_name, :last_name, :address, :city, :zip, :avatar_url, :about_me) }
 	end
 end
